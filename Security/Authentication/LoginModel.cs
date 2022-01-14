88d9e729092a372147c5f4fa02947f0e0eebd151
AhmedKosit <97341294+AhmedKosit@users.noreply.github.com>
2022-01-14 20:59:42 +0200

    Add files via upload

---
+++ Security/Authentication/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Security.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "BUID is required")]
+        public string BUID { get; set; }
+        public string Lang { get; set; }
+    }
+}
